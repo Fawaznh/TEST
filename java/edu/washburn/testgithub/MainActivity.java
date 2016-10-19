03898da64a877a7450e71ec7eb3f6ce5ca586580
Fawaznh <fawaznx@gmail.com>
2016-10-19 10:42:53 -0500

    test

--- java/edu/washburn/testgithub/MainActivity.java
+++ java/edu/washburn/testgithub/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i =111;
+        int i =000;
 
 
     }
